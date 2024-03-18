cf8723112ab23f2d08b91448c5609457f3506e05
andryusha2002 <andrey.fedorenko2002@gmail.com>
2024-03-18 23:29:29 +0200

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/CustomFileController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomFileController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Download(string customName, string customContent, string customFileName)
+        {
+            string fileContent = $"Custom Name: {customName}\nCustom Content: {customContent}";
+            string directoryPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "CustomFiles");
+
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            string filePath = Path.Combine(directoryPath, customFileName + ".txt");
+
+
+            System.IO.File.WriteAllText(filePath, fileContent, Encoding.UTF8);
+
+            return PhysicalFile(filePath, "text/plain", customFileName + ".txt");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
